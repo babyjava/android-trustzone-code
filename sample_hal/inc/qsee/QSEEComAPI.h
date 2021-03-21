3cc282f50f257264d2f141b65f7299a703573535
sheldon xie <q337527253@gmail.com>
2021-03-21 21:39:34 +0800

    hal v1.2

--- sample_hal/inc/qsee/QSEEComAPI.h
+++ sample_hal/inc/qsee/QSEEComAPI.h
@@ -1,20 +1,18 @@
-/*====*====*====*====*====*====*====*====*====*====*====*====*====*====*====*
-
-			HLOS QSEEComAPI Library Functions
-
-GENERAL DESCRIPTION
-  Contains the library functions for accessing QSEECom driver.
-
-EXTERNALIZED FUNCTIONS
-  None
-
-INITIALIZATION AND SEQUENCING REQUIREMENTS
-
-Copyright (c) 2010-2017 Qualcomm Technologies, Inc.
-All Rights Reserved.
-Qualcomm Technologies Confidential and Proprietary
-
-*====*====*====*====*====*====*====*====*====*====*====*====*====*====*====*/
+/*# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
 #ifndef __QSEECOMAPI_H_
 #define __QSEECOMAPI_H_
 
