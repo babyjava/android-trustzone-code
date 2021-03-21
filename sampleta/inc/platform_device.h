09c21d1107e7c462598f6028fd408279b2a18c2f
sheldon xie <q337527253@gmail.com>
2021-03-21 21:31:01 +0800

    v1.2

--- sampleta/inc/platform_device.h
+++ sampleta/inc/platform_device.h
@@ -1,3 +1,18 @@
+/*# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
 #define TA_VERSION "1.0.0.0"
 #define LOG_TAG "talog"
 #define LOG_RECORD snprintf(g_out->sys_err_line, sizeof(g_out->sys_err_line), LOG_TAG "err %s:%d", __FILE__, __LINE__);g_out->status = GENERIC_ERR;
--- sampleta/src/app_main.c
+++ sampleta/src/app_main.c
@@ -1,23 +1,18 @@
-/*
-  @file app_main.c
-  @brief App main entry point.
-
+/*# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 */
-/*===========================================================================
-  Copyright (c) 2020-2021 by XXX Technologies, Incorporated.  All Rights Reserved.
-  ===========================================================================*/
-
-/*=============================
-==============================================
-
-  EDIT HISTORY FOR FILE
-  $Header: talibv1/app_main.c
-  $Author: sheldon $
-
-  # when       who     what, where, why
-  # --------   ---     ---------------------------------------------------------
-  # 20/02/2020   sheldon     init
-  ===========================================================================*/
 #include "platform_tee.h"
 
 #ifdef TEE_QSEE
